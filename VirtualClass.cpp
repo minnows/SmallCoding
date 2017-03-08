00fe87344365f9f53f94728c16551aa1f228255d
Chen Ming <chenming228@hotmail.com>
2017-03-08 21:02:46 +0800

    test tech to run unit test

--- VirtualClass.cpp
+++ VirtualClass.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+// this is a virtual class
 class VirtualClass
 {
 public:
@@ -8,23 +9,46 @@ public:
 	virtual void PrintSelf()=0;
 };
 
+// this is child class inherented from virtual class
 class ChildClass:public VirtualClass
 {
 public:
 	void PrintSelf() { std::cout << "this is child class" << std::endl;};
 };
 
+// this is user class who has a member pointer to virtual base class
+// this member is initialized in instructor
 class UserClass
 {
 public:
 	UserClass(VirtualClass *virtualObj){m_virtualObj = virtualObj;};
+	void UserPrint()
+	{
+		m_virtualObj->PrintSelf();
+	};
 private:
 	VirtualClass *m_virtualObj;
 };
 
+// this is a test class with a member of child class object
+class TestClass
+{
+public:
+	TestClass(){user = new UserClass(&child);};
+	~TestClass(){delete user;};
+	void TestPrint()
+	{
+		user->UserPrint();
+	};
+private:
+	ChildClass child;
+	UserClass* user;
+	int temp;
+};
+
 int main()
 {
-	ChildClass child;
-	UserClass user(&child);
+	TestClass test;
+	test.TestPrint();
 	return 0;
 }
